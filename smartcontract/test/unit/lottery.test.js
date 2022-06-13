8321f49ad7e7b0d7426046e5501bdae4bed88bac
AlanRacciatti <alanracciatti1220@gmail.com>
2022-06-13 18:44:34 -0300

    test: add unit tests for lottery.sol

--- smartcontract/test/unit/lottery.test.js
+++ smartcontract/test/unit/lottery.test.js
@@ -24,6 +24,93 @@ describe("LotteryGame Unit Tests", () => {
       LotteryGame.address
     );
   });
+  it("Should NOT create Lottery is value equal to 0", async () => {
+    await expect(
+      lotteryGame.createLottery(ethers.utils.parseEther("0"), DAY * 5)
+    ).to.be.revertedWith("Ticket price must be greater than 0");
+  });
+  it("Should create Lottery", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.001"), DAY * 5);
+    const game = await lotteryGame.getLottery(0);
+    expect(game.prize).to.be.eq(ethers.utils.parseEther("0"));
+    expect(game.isFinished).to.be.eq(false);
+    expect(
+      Math.round(
+        (new Date(game.endDate * 1000).getTime() - new Date().getTime()) /
+          (1000 * 3600 * 24)
+      )
+    ).to.be.eq(5);
+  });
+  it("Should NOT participate if the price is lower than the required value", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.002"), DAY * 5);
+    await expect(
+      lotteryGame.participate(1, {
+        value: ethers.utils.parseEther("0.0015"),
+      })
+    ).to.be.revertedWith("Value must be equal to ticket price");
+  });
+  it("Should NOT participate if the price is higher than the required value", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0001"), DAY * 5);
+    await expect(
+      lotteryGame.participate(2, {
+        value: ethers.utils.parseEther("0.0002"),
+      })
+    ).to.be.revertedWith("Value must be equal to ticket price");
+  });
+  it("Should NOT participate if participation has already ended", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0001"), DAY * 5);
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 5]);
+    await expect(
+      lotteryGame.participate(3, {
+        value: ethers.utils.parseEther("0.0001"),
+      })
+    ).to.be.revertedWith("Lottery participation is closed");
+  });
+  it("Should participate", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await lotteryGame.connect(user2).participate(4, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await lotteryGame.connect(user3).participate(4, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    const game = await lotteryGame.getLottery(4);
+    expect(game.participants.length).to.be.eq(2);
+    expect(ethers.utils.formatEther(game.prize)).to.be.eq("0.001");
+  });
+  it("Should NOT declare winner if lottery still active", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await lotteryGame.connect(user2).participate(5, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await expect(lotteryGame.declareWinner(5)).to.be.revertedWith(
+      "Lottery is still active"
+    );
+  });
+  it("Should return the funds to the player if there is only one", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await lotteryGame.connect(user2).participate(6, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
+    await expect(await lotteryGame.declareWinner(6)).to.changeEtherBalance(
+      user2,
+      ethers.utils.parseEther("0.0005")
+    );
+  });
+  it("Should NOT declare the winner if missing Link funds", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await lotteryGame.connect(user2).participate(7, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await lotteryGame.connect(user3).participate(7, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
+    await expect(lotteryGame.declareWinner(7)).to.be.revertedWith(
+      "Not enough LINK"
+    );
+  });
   it("Should Declare a winner", async () => {
     const networkName = config[chainId].name;
     const additionalMessage = " --linkaddress " + linkToken.address;
@@ -42,19 +129,44 @@ describe("LotteryGame Unit Tests", () => {
     }
 
     await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
-    await lotteryGame.connect(user2).participate(0, {
+    await lotteryGame.connect(user2).participate(8, {
       value: ethers.utils.parseEther("0.0005"),
     });
-    await lotteryGame.connect(user3).participate(0, {
+    await lotteryGame.connect(user3).participate(8, {
       value: ethers.utils.parseEther("0.0005"),
     });
     await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
-    const tx = await lotteryGame.declareWinner(0);
+    const tx = await lotteryGame.declareWinner(8);
     const txReceipt = await tx.wait();
-    const requestId = txReceipt.events[2].topics[1];
 
+    /**
+     * events[0]: Transfer(address,address,uint256) from ERCBasic.sol
+     * events[1]: Transfer(address,address,uint256,bytes) from ERC677 called from ERC677Token.sol
+     * events[2]: RandomnessRequest(address,bytes32,uint256) from VRFCoordinatorMock.sol
+     * events[3]: WinnerDeclared from Lottery.sol
+     *
+     * Source: https://docs.soliditylang.org/en/v0.7.0/abi-spec.html
+     * Topic[0] is keccak(EVENT_NAME+"("+EVENT_ARGS.map(canonical_type_of).join(",")+")")
+     * Topic[1-3] are indexed input attributes
+     *
+     * The request Id is the second argument from the RandomnessRequest event
+     */
+    const requestId = txReceipt.events[2].topics[1];
     // eslint-disable-next-line no-unused-expressions
     expect(requestId).to.be.not.null;
+  });
+  it("Should emit an event when creating a lottery", async () => {
+    await expect(
+      lotteryGame.createLottery(ethers.utils.parseEther("0.001"), DAY * 1)
+    ).to.emit(lotteryGame, "LotteryCreated");
+  });
+  it("Should emit an event when participating in a lottery", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await expect(
+      lotteryGame.connect(user2).participate(10, {
+        value: ethers.utils.parseEther("0.0005"),
+      })
+    ).to.emit(lotteryGame, "PrizeIncreased");
   });
   it("Should emit an event when requesting randomness", async () => {
     const networkName = config[chainId].name;
@@ -74,16 +186,77 @@ describe("LotteryGame Unit Tests", () => {
     }
 
     await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
-    await lotteryGame.connect(user2).participate(1, {
+    await lotteryGame.connect(user2).participate(11, {
       value: ethers.utils.parseEther("0.0005"),
     });
-    await lotteryGame.connect(user3).participate(1, {
+    await lotteryGame.connect(user3).participate(11, {
       value: ethers.utils.parseEther("0.0005"),
     });
     await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
-    await expect(lotteryGame.declareWinner(1)).to.emit(
+    await expect(lotteryGame.declareWinner(11)).to.emit(
       lotteryGame,
       "RandomnessRequested"
+    );
+  });
+  it("Should NOT create lottery if NOT ADMIN", async () => {
+    await expect(
+      lotteryGame
+        .connect(user2)
+        .createLottery(ethers.utils.parseEther("0.001"), DAY * 1)
+    ).to.be.revertedWith("Only callable by admin");
+  });
+  it("Should NOT declare winner if NOT ADMIN", async () => {
+    const networkName = config[chainId].name;
+    const additionalMessage = " --linkaddress " + linkToken.address;
+    if (
+      await autoFundCheck(
+        lotteryGame.address,
+        networkName,
+        linkToken.address,
+        additionalMessage
+      )
+    ) {
+      await hre.run("fund-link", {
+        contract: lotteryGame.address,
+        linkaddress: linkToken.address,
+      });
+    }
+
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await lotteryGame.connect(user2).participate(12, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
+    await expect(
+      lotteryGame.connect(user2).declareWinner(12)
+    ).to.be.revertedWith("Only callable by admin");
+  });
+  it("Should return the funds to the player if there is only one even if it has partcipated more than once", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await lotteryGame.connect(user2).participate(13, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await lotteryGame.connect(user2).participate(13, {
+      value: ethers.utils.parseEther("0.0005"),
+    });
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
+    await expect(await lotteryGame.declareWinner(13)).to.changeEtherBalance(
+      user2,
+      ethers.utils.parseEther("0.001")
+    );
+  });
+  it("Should finish the lottery even if no one has participated", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
+    await lotteryGame.declareWinner(14);
+    expect((await lotteryGame.getLottery(14)).isFinished).to.be.true;
+  });
+  it("Should emit an event when declaring winner and there's only one participant or no participants", async () => {
+    await lotteryGame.createLottery(ethers.utils.parseEther("0.0005"), DAY * 1);
+    await ethers.provider.send("evm_increaseTime", [3600 * 24 * 2]);
+    await expect(lotteryGame.declareWinner(15)).to.emit(
+      lotteryGame,
+      "LotteryFinished"
     );
   });
 });
